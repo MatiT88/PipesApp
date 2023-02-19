b385ef1e6657ba4b4d766ea4c3dac659cec3122d
MatiT88 <matiastecici@gmail.com>
2023-02-19 13:03:32 -0300

    Se creo la page basicos

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+  nombreLower: string = 'matias';
+  nombreUpper: string = 'MATIAS';
+  nombreCompleto: string = 'mAtiAs tECici';
+
+
 }
