8aedf3df30034af570a022ce7b8fcf2475c2aece
MatiT88 <matiastecici@gmail.com>
2023-02-20 12:49:12 -0300

    Timezone y otros idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule,LOCALE_ID } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { AppRouterModule } from './app-router.module';
 import { SharedModule } from "./shared/shared.module";
 import { VentasModule } from './ventas/ventas.module';
 
-
+//Cambiar el locale de la app
+import localeEs from "@angular/common/locales/es-AR";
+import localeHe from "@angular/common/locales/he";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localeEs);
+registerLocaleData(localeHe);
 
 @NgModule({
   declarations: [
@@ -18,7 +23,9 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
