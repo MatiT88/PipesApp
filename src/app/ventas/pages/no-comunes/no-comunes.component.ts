90e4e9932296084db79b6834fcb3f8eb768a5f92
MatiT88 <matiastecici@gmail.com>
2023-02-21 14:49:22 -0300

    Se implementaron asyn pipes en el no-comunes.component

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 
 @Component({
@@ -61,6 +62,14 @@ export class NoComunesComponent {
       nombre: 'Aquaman',
       vuela: false
     },
-  ]
+  ];
 
+  //Async Pipe
+  miObservable = interval(5000)
+
+  valorPromesa = new Promise( (resolve, reject ) => {
+    setTimeout(() => {
+      resolve( 'Tenemos data de una promesa')
+    }, 3500);
+  } );
 }
