45bd91a6f3186c00c93ae64b227dce344599725f
MatiT88 <matiastecici@gmail.com>
2023-02-23 13:04:24 -0300

    Se creo el pipe ordenar.pipe en la carpeta ventas/pipes y se lo implemento en el componente ordenar.component

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
+import { BasicosComponent } from './pages/basicos/basicos.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
-import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
+import { OrdenarPipe } from './pipes/ordenar.pipe';
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
 import { VuelaPipe } from './pipes/vuela.pipe';
 
@@ -14,11 +15,14 @@ import { VuelaPipe } from './pipes/vuela.pipe';
 
 @NgModule({
   declarations: [
+    //Components
+    BasicosComponent,
     NumerosComponent,
     NoComunesComponent,
-    BasicosComponent,
     OrdenarComponent,
+    //Pipes
     MayusculasPipe,
+    OrdenarPipe,
     VuelaPipe
   ],
   exports:[
